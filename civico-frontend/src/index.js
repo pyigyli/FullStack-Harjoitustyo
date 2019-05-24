360cab118d0f25593639351594f16d6fa272da53
pyigyli <pyry.vahakangas@luukku.com>
2019-05-24 22:02:47 +0300

    index.js palautettu edelliseen committiin

--- civico-frontend/src/index.js
+++ civico-frontend/src/index.js
@@ -1,19 +1,4 @@
-const path = require('path');
-const express = require('express');
-
-const app = express();
-
-const distFolder = path.resolve(__dirname, '../build');
-
-app.use(express.static(path.resolve(distFolder, 'client')));
-
-// Fallback to index.html
-app.get(/^(\/.*)?$/, (_req, res) => {
-	res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-	res.header('Pragma', 'no-cache');
-	res.header('Expires', '0');
-	res.sendFile(path.resolve(distFolder, 'client/index.html'));
-});
+const app = require('./app');
 
 const port = parseInt(process.env.PORT || '8888', 10);
 app.listen(port, () => {
