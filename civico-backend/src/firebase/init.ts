78c31fc5e840073e8b47d445d1c6b21775720a6e
pyigyli <pyry.vahakangas@luukku.com>
2019-05-25 20:56:59 +0300

    firebase config no longer gitignored

--- civico-backend/src/firebase/init.ts
+++ civico-backend/src/firebase/init.ts
@@ -1,11 +1,9 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
-import {apiKey, databaseURL} from './secret'
-// secret.ts is ignored by git
 
 const config = {
-  apiKey,
-  databaseURL
+  apiKey: 'AAAAEL_zR7E:APA91bHpUBbYRpG0rbkYJG3WALNQTPwKn2oW-U8hC-veNev_IzJQNvfWzvL7CapVSG7O-IYl6Nn9Rxkdf1lxEf9tVKi9JGjwvItXG3Rh_JazmlmMQdBl9wjgskhjOhA7P-FfHVgalXDb',
+  databaseURL: 'https://civico-database.firebaseio.com'
 }
 
 firebase.initializeApp(config)
